fc4f8774fec54fbbf59cdc54e9c8eeba2accf667
Hazel Lobos <hazel@lobos.ca>
2016-11-02 14:59:40 -0600

    Added Images For Real for VS

--- DogWalkies/obj/Debug/android/src/dogwalkies/dogwalkies/R.java
+++ DogWalkies/obj/Debug/android/src/dogwalkies/dogwalkies/R.java
@@ -13,13 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int button1=0x7f050001;
+        public static final int button2=0x7f050002;
+        public static final int imageButton1=0x7f050003;
+        public static final int imageView1=0x7f050004;
+        public static final int linearLayout1=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int ApplicationName=0x7f040000;
-    }
-    public static final class style {
-        public static final int Theme_NoTitle=0x7f050000;
     }
 }

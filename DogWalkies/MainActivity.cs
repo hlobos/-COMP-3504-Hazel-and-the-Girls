c661f0dfc695519d6e76015c0a64927d3367b3bb
hlobo006 <hlobo006@mtroyal.ca>
2016-11-11 15:51:18 -0700

    Adding branch/commit instructions to MainActivity

--- DogWalkies/MainActivity.cs
+++ DogWalkies/MainActivity.cs
@@ -11,7 +11,12 @@ namespace DogWalkies
     [Activity(Label = "DogWalkies", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        
+        //Switch to: SYNC,sync your master branch (make sure all is up-to-date)
+        //Switch to: BRANCH, checkout to your 'develop' branch MAKE YOUR CHANGES HERE
+        //Switch to: CHANGES, type in your commit comment > Commit All and Push
+        //Switch to: BRANCHES, merge changes from 'develop' branch to your 'master' branch
+        //Swtich to: SYNC, ... changes from master to origin
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -19,7 +24,6 @@ namespace DogWalkies
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            //Checking in on Hazel Branch Test
         }
     }
 }

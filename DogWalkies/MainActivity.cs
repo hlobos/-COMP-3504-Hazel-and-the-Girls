b8ba8d1dd928945216590586cd012a4a434752c5
hlobo006 <hlobo006@mtroyal.ca>
2016-11-12 20:54:38 -0700

    Removed Github Comment in MainActivity

--- DogWalkies/MainActivity.cs
+++ DogWalkies/MainActivity.cs
@@ -13,13 +13,6 @@ namespace DogWalkies
     [Activity(Label = "DogWalkies", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        //START: 'master' branch
-        //Switch to: SYNC, sync your master branch (make sure all is up-to-date)
-        //Switch to: BRANCHES, double-click your 'develop' branch MAKE YOUR CHANGES HERE
-        //Switch to: CHANGES, type in your commit comment > Commit All and Push
-        //Switch to: BRANCHES, double-click your 'master' branch, merge changes from 'develop' to 'master'
-        //Switch to: SYNC, push your commit from 'master' to the origin 
-        //[REPEAT]
 
         protected override void OnCreate(Bundle bundle)
         {

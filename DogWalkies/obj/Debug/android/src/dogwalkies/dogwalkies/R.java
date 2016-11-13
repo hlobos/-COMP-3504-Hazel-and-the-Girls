e6890eadfa710da88f2e2f619f8f1ed47e3c8a66
hlobo006 <hlobo006@mtroyal.ca>
2016-11-12 18:51:38 -0700

    Updating Local Master

--- DogWalkies/obj/Debug/android/src/dogwalkies/dogwalkies/R.java
+++ DogWalkies/obj/Debug/android/src/dogwalkies/dogwalkies/R.java
@@ -12,15 +12,20 @@ public final class R {
     }
     public static final class drawable {
         public static final int addcircleicon=0x7f020000;
-        public static final int addicon=0x7f020001;
-        public static final int button=0x7f020002;
-        public static final int buttonenabled=0x7f020003;
-        public static final int buttonpressed=0x7f020004;
-        public static final int cameraicon=0x7f020005;
-        public static final int dogwalkieslogo=0x7f020006;
-        public static final int icon=0x7f020007;
-        public static final int pawicon=0x7f020008;
-        public static final int sheltiemainview=0x7f020009;
+        public static final int button=0x7f020001;
+        public static final int buttonenabled=0x7f020002;
+        public static final int buttonpressed=0x7f020003;
+        public static final int cameraicon=0x7f020004;
+        public static final int dogwalkieslogo=0x7f020005;
+        public static final int drafticon=0x7f020006;
+        public static final int editicon=0x7f020007;
+        public static final int editicon2=0x7f020008;
+        public static final int homeicon=0x7f020009;
+        public static final int icon=0x7f02000a;
+        public static final int imageicon=0x7f02000b;
+        public static final int infoicon=0x7f02000c;
+        public static final int pawicon=0x7f02000d;
+        public static final int sheltiemainview=0x7f02000e;
     }
     public static final class id {
         public static final int ButtonProfile=0x7f050007;

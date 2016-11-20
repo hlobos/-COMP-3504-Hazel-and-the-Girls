b48d83dc6a7b2818f065345efced2aa6c7d673b1
Marina Vu <plain.abstract@hotmail.com>
2016-11-19 22:03:08 -0700

    Add missing files from previous commit

--- DogWalkies/Resources/Resource.Designer.cs
+++ DogWalkies/Resources/Resource.Designer.cs
@@ -114,6 +114,9 @@ namespace DogWalkies
 			// aapt resource value: 0x7f050009
 			public const int ButtonStartWalk = 2131034121;
 			
+			// aapt resource value: 0x7f05000e
+			public const int DogNameBar = 2131034126;
+			
 			// aapt resource value: 0x7f050007
 			public const int ImageButtonAddDogProfileImage = 2131034119;
 			
@@ -128,6 +131,9 @@ namespace DogWalkies
 			
 			// aapt resource value: 0x7f050006
 			public const int ImageViewDogProfile = 2131034118;
+			
+			// aapt resource value: 0x7f05000f
+			public const int ImageViewPaw = 2131034127;
 			
 			// aapt resource value: 0x7f050002
 			public const int ImageViewPawPrintIcon = 2131034114;
@@ -144,8 +150,56 @@ namespace DogWalkies
 			// aapt resource value: 0x7f050003
 			public const int TextViewDogFirstName = 2131034115;
 			
+			// aapt resource value: 0x7f050015
+			public const int editText1 = 2131034133;
+			
+			// aapt resource value: 0x7f05001e
+			public const int imageButton1 = 2131034142;
+			
+			// aapt resource value: 0x7f05001a
+			public const int imageView2 = 2131034138;
+			
+			// aapt resource value: 0x7f050011
+			public const int linearLayout1 = 2131034129;
+			
+			// aapt resource value: 0x7f050018
+			public const int linearLayout2 = 2131034136;
+			
+			// aapt resource value: 0x7f050016
+			public const int linearLayout3 = 2131034134;
+			
+			// aapt resource value: 0x7f050019
+			public const int linearLayout4 = 2131034137;
+			
 			// aapt resource value: 0x7f050000
 			public const int mainScrollView = 2131034112;
+			
+			// aapt resource value: 0x7f05000d
+			public const int metricsScrollView = 2131034125;
+			
+			// aapt resource value: 0x7f050013
+			public const int monthButton = 2131034131;
+			
+			// aapt resource value: 0x7f05001b
+			public const int noteButton = 2131034139;
+			
+			// aapt resource value: 0x7f05001d
+			public const int notesTextInput = 2131034141;
+			
+			// aapt resource value: 0x7f05001c
+			public const int relativeLayout2 = 2131034140;
+			
+			// aapt resource value: 0x7f050010
+			public const int relativeLayout3 = 2131034128;
+			
+			// aapt resource value: 0x7f050012
+			public const int weekButton = 2131034130;
+			
+			// aapt resource value: 0x7f050017
+			public const int weightEditButton = 2131034135;
+			
+			// aapt resource value: 0x7f050014
+			public const int yearButton = 2131034132;
 			
 			static Id()
 			{
@@ -162,6 +216,9 @@ namespace DogWalkies
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Metrics = 2130903041;
 			
 			static Layout()
 			{
